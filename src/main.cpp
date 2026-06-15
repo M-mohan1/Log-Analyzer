e4a4c677bde5fdc5535ddd124a43612f465f7244
M-mohan1 <mohitmohanyadav481@gmail.com>
2026-06-15 02:09:45 +0000

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,10 @@ private:
     vector<LogEntry> logs;
     unordered_map<string, int> levelCount;
     unordered_map<string, int> messageCount;
+    unordered_map<string, int> patternCount;
+    unordered_map<string, int> hourCount;
+    unordered_map<int, int> errorPerHour;
+    vector<string> errorTimestamps;
 
     LogEntry parseLine(const string &line) {
     LogEntry entry;
@@ -61,6 +65,40 @@ private:
     return entry;
 }
 
+string extractPattern(const string &msg) {
+    string lower;
+
+    // convert to lowercase
+    for (char c : msg) {
+        lower += tolower(c);
+    }
+
+    // pattern rules (simple but powerful)
+    if (lower.find("disk") != string::npos) return "disk issue";
+    if (lower.find("memory") != string::npos) return "memory issue";
+    if (lower.find("cpu") != string::npos) return "cpu issue";
+    if (lower.find("database") != string::npos) return "database issue";
+    if (lower.find("login") != string::npos) return "login activity";
+    if (lower.find("security") != string::npos) return "security event";
+    if (lower.find("network") != string::npos) return "network issue";
+    if (lower.find("backup") != string::npos) return "backup process";
+    if (lower.find("error") != string::npos) return "generic error";
+
+    return "other";
+}
+
+string extractHour(const string &timestamp) {
+    if (timestamp.size() < 13) return "UNKNOWN";
+
+    return timestamp.substr(11, 2); // HH from YYYY-MM-DD HH:MM:SS
+}
+int ExtractHour(const string &timestamp) {
+    if (timestamp.size() < 13) return -1;
+
+    // HH is at index 11 and 12
+    return stoi(timestamp.substr(11, 2));
+}
+
 public:
     // Loads and parses the file, updating statistics in real-time
     bool loadFile(const string &filename) {
@@ -76,6 +114,21 @@ public:
             logs.push_back(entry);
             levelCount[entry.level]++; // $O(1)$ update during read
             messageCount[entry.message]++;
+            string pattern = extractPattern(entry.message);
+            patternCount[pattern]++;
+            string hour = extractHour(entry.timestamp);
+            hourCount[hour]++;
+
+            if (entry.level == "ERROR") {
+    int hour = ExtractHour(entry.timestamp);
+    if (hour != -1) {
+        errorPerHour[hour]++;
+    }
+}
+
+if (entry.level == "ERROR") {
+    errorTimestamps.push_back(entry.timestamp);
+}
         }
         file.close();
         return true;
@@ -139,6 +192,39 @@ void printTopKMessages(int k) {
             << " times\n";
     }
 }
+int convertToMinutes(const string &timestamp) {
+    int hour = stoi(timestamp.substr(11, 2));
+    int minute = stoi(timestamp.substr(14, 2));
+    return hour * 60 + minute;
+}
+
+void detectBurstErrors(int windowSize, int threshold) {
+    cout << "\n=== Burst Error Detection ===\n";
+
+    vector<int> times;
+
+    for (auto &t : errorTimestamps) {
+        times.push_back(convertToMinutes(t));
+    }
+
+    sort(times.begin(), times.end());
+
+    int left = 0;
+
+    for (int right = 0; right < times.size(); right++) {
+        while (times[right] - times[left] > windowSize) {
+            left++;
+        }
+
+        int count = right - left + 1;
+
+        if (count >= threshold) {
+            cout << "🚨 ALERT: "
+                << count << " errors within "
+                << windowSize << " minutes\n";
+        }
+    }
+}
 
 void searchLogs(string keyword) {
     cout << "\n--- Search Results for: \"" << keyword << "\" ---\n";
@@ -172,6 +258,67 @@ void searchLogs(string keyword) {
         cout << "Total matches found: " << matchCount << endl;
     }
 }
+void printPatternStats() {
+    cout << "\n===== PATTERN ANALYSIS =====\n";
+
+    for (auto &p : patternCount) {
+        cout << p.first << " → " << p.second << endl;
+    }
+}
+void printTimeAnalysis() {
+    cout << "\n===== TIME ANALYSIS =====\n";
+
+    for (auto &p : hourCount) {
+        cout << p.first << ":00 → " << p.second << " logs\n";
+    }
+}
+void printPeakHour() {
+    string peakHour;
+    int maxCount = 0;
+
+    for (auto &p : hourCount) {
+        if (p.second > maxCount) {
+            maxCount = p.second;
+            peakHour = p.first;
+        }
+    }
+
+    cout << "\n🔥 Peak Log Hour: " << peakHour 
+        << ":00 with " << maxCount << " logs\n";
+}
+void printErrorByHour() {
+    cout << "\n=== Error Distribution by Hour ===\n";
+
+    for (auto &p : errorPerHour) {
+        cout << p.first << ":00 - " << p.second << " errors\n";
+    }
+}
+void printPeakErrorHour() {
+    int maxHour = -1;
+    int maxCount = 0;
+
+    for (auto &p : errorPerHour) {
+        if (p.second > maxCount) {
+            maxCount = p.second;
+            maxHour = p.first;
+        }
+    }
+
+    cout << "\n🚨 Peak Error Hour: " 
+        << maxHour << ":00 with " 
+        << maxCount << " errors\n";
+}
+void detectAnomalies(int threshold) {
+    cout << "\n=== Anomaly Detection ===\n";
+
+    for (auto &p : errorPerHour) {
+        if (p.second > threshold) {
+            cout << "🚨 ALERT: High error rate at "
+                << p.first << ":00 ("
+                << p.second << " errors)\n";
+        }
+    }
+}
 };
 
 int main() {
@@ -191,15 +338,21 @@ int main() {
     // Display total statistics
     analyzer.printStats();
     analyzer.printTopKMessages(3);
-
+//     analyzer.printPatternStats();
+//     analyzer.printTimeAnalysis();
+//     analyzer.printPeakHour();
+//     analyzer.printErrorByHour();
+// analyzer.printPeakErrorHour();
+analyzer.detectAnomalies(2); // try 2 or 3
+analyzer.detectBurstErrors(5,1); 
     // 2. Ask user for a search term
-    string searchTerm;
-    cout << "\nEnter a keyword to search in logs (or 'exit' to quit): ";
-    getline(cin, searchTerm);
+    //string searchTerm;
+    //cout << "\nEnter a keyword to search in logs (or 'exit' to quit): ";
+    //getline(cin, searchTerm);
 
-    if (searchTerm != "exit" && !searchTerm.empty()) {
-        analyzer.searchLogs(searchTerm);
-    }
+    //if (searchTerm != "exit" && !searchTerm.empty()) {
+     //   analyzer.searchLogs(searchTerm);
+   // }
 
     return 0;
 }
